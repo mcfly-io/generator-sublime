bdb122dcb8d1521f3cf7b4fdc919705aa0c4f7d7
thaiat <thaiat@ipelia.fr>
2014-08-28 14:32:58 +0000

    Upgrade to latest version of yeoman
    Update package.json
    Fix require chalk in lint.js
    Fix falling unit test on travis
    Reorder generator methods using the new placeholders
    Upgrade subgenerator bash
    Upgrade unit test for app
    Delete create test as they are not required anymore

--- test/bash.create.tests.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var assert = require('assert');
-
-describe('sublime bash subgenerator', function() {
-    it('can be imported without blowing up', function() {
-        var bash = require('../bash');
-        assert(bash !== undefined);
-    });
-});
