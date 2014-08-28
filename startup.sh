1caa1a08344cfa10576bf93f29b3dab05c79a16e
thaiat <thaiat@ipelia.fr>
2014-08-28 07:31:25 +0000

    Add a variable to retain current directory

--- startup.sh
+++ startup.sh
@@ -6,6 +6,11 @@ TRAVIS_VERSION="1.7.1"
 
 clear
 
+CURRENT_DIRECTORY=$(pwd)
+
+# configure git
+bash ./deploy/git-config.sh
+
 # install node
 source ~/.nvm/nvm.sh
 nvm install $NODE_VERSION
@@ -16,14 +21,14 @@ node --version
 curl -L http://install.ohmyz.sh | sh
 cd ~/.oh-my-zsh/custom/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+cd $CURRENT_DIRECTORY
 # forces terminal to use zsh
 echo "zsh && exit 0" >> ~/.bash_profile
 
 # install travis
 gem install travis -v $TRAVIS_VERSION --no-rdoc --no-ri
 
-# configure git
-bash ./deploy/git-config.sh
+
 
 
 
--- templates/root/startup.sh
+++ templates/root/startup.sh
@@ -6,6 +6,13 @@ TRAVIS_VERSION="1.7.1"
 
 clear
 
+CURRENT_DIRECTORY=$(pwd)
+
+<% if (Gitconfig) { %>
+# configure git
+bash ./deploy/git-config.sh
+<% }%>
+
 # install node
 source ~/.nvm/nvm.sh
 nvm install $NODE_VERSION
@@ -16,13 +23,10 @@ node --version
 curl -L http://install.ohmyz.sh | sh
 cd ~/.oh-my-zsh/custom/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+cd $CURRENT_DIRECTORY
 # forces terminal to use zsh
 echo "zsh && exit 0" >> ~/.bash_profile
 
 # install travis
 gem install travis -v $TRAVIS_VERSION --no-rdoc --no-ri
 
-<% if (Gitconfig) { %>
-# configure git
-bash ./deploy/git-config.sh
-<% }%>
