ec7e670191a9420e4353adfc6c299e4911a768e6
thaiat <thaiat@ipelia.fr>
2014-08-30 16:58:45 +0000

    Simplify CI

--- deploy/prepublish.sh
+++ deploy/prepublish.sh
@@ -4,4 +4,5 @@ bash which yo || npm install -g yo
 bash which istanbul || npm install -g istanbul
 bash which mocha || npm install -g mocha
 bash which gulp || npm install -g gulp
+bash which codeclimate || npm install -g codeclimate-test-reporter
 echo "prepublish executed sucessfully"
