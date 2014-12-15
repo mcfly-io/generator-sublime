9bcb5012d66de0d123658ca747ca3e7bf54b71ac
Avi Haiat <thaiat@ipelia.fr>
2014-12-15 14:22:42 +0100

    feat(gulps): Change dist folder to include mode

--- templates/gulps/common/constants.js
+++ templates/gulps/common/constants.js
@@ -64,7 +64,7 @@ module.exports = function() {
             timeout: 5000
         },
         dist: {
-            distFolder: './dist/{{targetName}}/'
+            distFolder: './dist/{{targetName}}/{{mode}}'
         }
     };
 
