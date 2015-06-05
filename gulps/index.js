c3559ecfe0cb2f67f2f05e78c24a299114fefb6f
Avi Haiat <thaiat@ipelia.fr>
2015-06-05 23:08:30 +0300

    fix(gulps): Use fork for babelify

--- gulps/index.js
+++ gulps/index.js
@@ -22,7 +22,7 @@ var GulpsGenerator = yeoman.generators.Base.extend({
         ];
 
         this.npmPackagesVersion = {
-            'babelify': '6.1.1',
+            'babelify': 'thaiat/babelify',  // TODO: revert to babelify when issue #88 is closed
             'brfs': '1.4.0',
             'browser-sync': '2.2.4',
             'browserify': '9.0.3',
