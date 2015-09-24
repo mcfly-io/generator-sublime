8d9aa7ce20bc7112c2690beaebbb19db5c458bbc
Avi Haiat <thaiat@ipelia.fr>
2015-09-24 18:51:40 +0300

    fix(gulps): Remove <% from serve.js

--- gulp_tasks/common/constants.js
+++ gulp_tasks/common/constants.js
@@ -4,7 +4,7 @@ module.exports = function() {
     var constants = {
         repository: 'https://github.com/mcfly-io/generator-sublime',
         versionFiles: ['./package.json', './bower.json'],
-        lint: ['app/**/*.js', 'bash/**/*.js', 'gulps/**/*.js', 'templates/gulps/tasks/**/*.js', '!templates/gulps/tasks/serve.js', 'gulpfile.js', 'gulp_tasks/**/*.js', 'karma.conf.js', 'test/**/*.js'],
+        lint: ['app/**/*.js', 'bash/**/*.js', 'gulps/**/*.js', 'templates/gulps/tasks/**/*.js', 'gulpfile.js', 'gulp_tasks/**/*.js', 'karma.conf.js', 'test/**/*.js'],
         mocha: {
             libs: ['app/**/*.js', 'gulps/**/*.js', 'bash/**/*.js', 'class/**/*.js'],
             tests: ['test/**/*.js'],
